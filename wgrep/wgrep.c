0559ee968fd20ae4317440f79907eb67299a8399
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-12-29 20:37:54 +0200

    Wrgrep third edition search in all lines and in all files

--- wgrep/wgrep.c
+++ wgrep/wgrep.c
@@ -7,32 +7,39 @@ int main(int argc, char *argv[]){
     FILE * filePtr = NULL;
     size_t bufferSize = 0;
     char * searchResult = NULL;
-
+    int numberOfFiles = 0;
+    
     if(argc < 2){
         printf("wgrep: searchterm [file ...]\n");
         return 1;
-    } else if (argc < 3) {
-        /* Read from STDIN */
+    } else if(argc < 3){
         filePtr = stdin;
+        numberOfFiles = 1;
     } else {
-        /* Read from the files */
+        numberOfFiles = argc - 2;
         filePtr = fopen(argv[2], "r");
-    
+    }
+
+    for(int i = 0;i < numberOfFiles;i++){
+        if(numberOfFiles != 1){
+            filePtr = fopen(argv[i + 2], "r");
+        }
+
         if(filePtr == NULL){
             printf("wgrep: cannot open file\n");
             return 1;
         }
+
+        while (getline(&linePtr, &bufferSize, filePtr) != -1){
+            searchResult = strstr(linePtr, argv[1]);
+            if (searchResult != NULL){
+                printf("%s", linePtr);
+            }
+        }
+        fclose(filePtr);
     }
 
-    while (getline(&linePtr, &bufferSize, filePtr) != -1){
-        searchResult = strstr(linePtr, argv[1]);
-        if (searchResult != NULL)
-        {
-            printf("%s", linePtr);
-        }
-    }
     free(linePtr);
-    fclose(filePtr);
 
     return 0;
 }
