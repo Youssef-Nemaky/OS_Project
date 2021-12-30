3bd8a58feaa8929c41909a0c526e5286fb26896d
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-12-30 16:51:23 +0200

    all lines in all files but reading each file alone

--- wzip/wzip.c
+++ wzip/wzip.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]){
     char * start = NULL;
     char * end = NULL;
     int runLength = 1;
+    int firstLine = 1;
     size_t lineSize = 0;
 
     if(argc < 2){
@@ -15,31 +16,39 @@ int main(int argc, char *argv[]){
         return 1;
     } 
 
-    filePtr = fopen(argv[1], "r");
+    for (int i = 0; i < argc - 1; i++){
+        firstLine = 1;
+        filePtr = fopen(argv[i + 1], "r");
+        if (filePtr == NULL){
+            printf("wzip: cannot open file\n");
+            return 1;
+        }
 
-    if(filePtr == NULL){
-        printf("wzip: cannot open file\n");
-        return 1;
-    }
+        while (getline(&linePtr, &lineSize, filePtr) != -1){
 
-    while(getline(&linePtr,&lineSize,filePtr) != -1){
+            /* now linePtr is like an array of string containing the line which fgets read */
+            start = linePtr;
+            end = linePtr + 1;
 
-        /* now linePtr is like an array of string containing the line which fgets read */
-        start = linePtr;
-        end = linePtr + 1;
-
-        while ((*start != '\n') && (*start != '\r') && (*start != ' ')){
-            runLength = 1;
-            while (*end == *start){
-                runLength++;
+            while ((*start != '\0') && (*start != '\n') && (*start != '\r') && (*start != ' ')){
+                if(firstLine == 1){
+                    firstLine = 0;
+                } else {
+                    fwrite("\n", sizeof(char), sizeof(char), stdout);
+                }
+                runLength = 1;
+                while (*end == *start){
+                    runLength++;
+                    end++;
+                }
+                fwrite(&runLength, 1, sizeof(int), stdout);
+                fwrite(start, 1, sizeof(char), stdout);
+                start = end;
                 end++;
             }
-            fwrite(&runLength, 1, sizeof(int), stdout);
-            fwrite(start, 1, sizeof(char), stdout);
-            start = end;
-            end++;
         }
-        fwrite("\n",sizeof(char),sizeof(char),stdout);
+        free(linePtr);
+        fclose(filePtr);
     }
 
     return 0;
